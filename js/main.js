da56a2f4e11e5e7fdef0f734c414618dd09c8bc1
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-03-11 22:43:35 +0400

    feat:add functional in modal

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  const modal = document.querySelector('.modal');
+  const modalDialog = document.querySelector('.modal__dialog');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  }
+
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal );
+  });
+    closeBtn.addEventListener('click', switchModal);
+   // modal.addEventListener('click', switchModal);
+
+});
