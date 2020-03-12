7865a5d5f6592268367ca6c49f0be4383431d7bf
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-03-12 15:49:51 +0400

    feat:add buutton scroll-top. reafact:change code js from jquery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+/*
 document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
   const modalDialog = document.querySelector('.modal__dialog');
@@ -13,4 +14,48 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
     closeBtn.addEventListener('click', switchModal);
 
+});
+*/
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalDialog = $('.modal__dialog'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function() {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function() {
+    modal.toggleClass('modal--visible');
+  });
+  modal.mouseup(function (e){
+    if (!modalDialog.is(e.target) 
+      && modalDialog.has(e.target).length === 0) {
+      modal.toggleClass('modal--visible');
+  }
+  });
+  
+});
+
+
+$(this).keydown(function(eventObject){
+  if (eventObject.which == 27)
+      $('.modal').removeClass('modal--visible');
+});
+
+
+var scrollTop = $('.scroll-top');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    scrollTop.addClass('show');
+  } else {
+    scrollTop.removeClass('show');
+  }
+});
+
+scrollTop.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '1500');
 });
